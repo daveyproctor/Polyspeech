7709e4a5feb710e6947354fe8be87bce6ba67352
Davey Proctor <david.proctor@yale.edu>
2019-02-25 18:46:46 -0500

    took ~1hr to run 6 topic models.

--- progress.py
+++ progress.py
@@ -1,11 +1,14 @@
+import time
+
 class ProgressTracker(object):
     def __init__(self, maxCount, everyPercent=5):
         self.maxCount = maxCount
         self.soFar = 0
         self.everyPercent = everyPercent
+        self.start = time.time()
         
     def update(self, i):
         percentDone = (i/self.maxCount * 100)
         if self.soFar + self.everyPercent < percentDone:
             self.soFar += self.everyPercent
-            print("{} percent done. i: {}".format(self.soFar, i))
+            print("{} percent done. i: {}. Time Elapsed: {}".format(self.soFar, i, time.time()-self.start))
