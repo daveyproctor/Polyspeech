559e2efbddcf494caf84db7fb8b287e0f5e00e79
Davey Proctor <david.proctor@yale.edu>
2019-02-26 00:19:52 -0500

    screwed around w topic models. more IBM spike

--- progress.py
+++ progress.py
@@ -6,8 +6,13 @@ class ProgressTracker(object):
         self.soFar = 0
         self.everyPercent = everyPercent
         self.start = time.time()
+        self.internalI = 0
         
-    def update(self, i):
+    def update(self, i=None):
+        if i is None:
+            # can call wo argument
+            self.internalI += 1
+            i = self.internalI
         percentDone = (i/self.maxCount * 100)
         if self.soFar + self.everyPercent < percentDone:
             self.soFar += self.everyPercent
